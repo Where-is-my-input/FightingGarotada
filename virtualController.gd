5647d74d7104e5e4787ec34b7e2684186c5648b3
Ikkisoad <henrique.alberto.andrade@hotmail.com>
2024-06-08 18:49:25 -0300

    Gattling v1 L > M > H

--- virtualController.gd
+++ virtualController.gd
@@ -84,7 +84,7 @@ func _process(_delta):
 	directionX = Input.get_action_strength(right)-Input.get_action_strength(left)
 	directionY = Input.get_action_strength(down)-Input.get_action_strength(up)
 	
-	readButtons()
+	readButtons() #change to input event to not call everyframe?
 	modulateVisualController()
 
 func readButtons():
