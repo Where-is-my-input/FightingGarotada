82d50f64280eb8cfa35ecbb0de31b4b91e82e797
Ikkisoad <henrique.alberto.andrade@hotmail.com>
2024-06-13 23:45:56 -0300

    Color palette text #46

--- Scenes/paletteEditor/colorSlider.gd
+++ Scenes/paletteEditor/colorSlider.gd
@@ -1,8 +1,11 @@
 extends HBoxContainer
 @onready var color_rect = $ColorRect
-@onready var r = $sliders/R
-@onready var g = $sliders/G
-@onready var b = $sliders/B
+@onready var r = $sliders/boxR/R
+@onready var g = $sliders/boxG/G
+@onready var b = $sliders/boxB/B
+@onready var line_r = $sliders/boxR/lineR
+@onready var line_g = $sliders/boxG/lineG
+@onready var line_b = $sliders/boxB/lineB
 
 signal changed()
 
@@ -10,21 +13,24 @@ func _ready():
 	r.value = color_rect.color.r * 255
 	g.value = color_rect.color.g * 255
 	b.value = color_rect.color.b * 255
+	line_r.text = str(r.value)
+	line_g.text = str(g.value)
+	line_b.text = str(b.value)
 
 func _on_r_drag_ended(value_changed):
-	var rr = r.value / 255
-	color_rect.color.r = rr
+	color_rect.color.r = colorChanged(r, line_r)
+
+func colorChanged(slider, text):
+	var rr = slider.value / 255
+	text.text = str(rr * 255)
 	changed.emit(self, rr)
+	return rr
 
 func _on_g_drag_ended(value_changed):
-	var gg = g.value / 255
-	color_rect.color.g = gg
-	changed.emit(self, gg)
+	color_rect.color.g = colorChanged(g, line_g)
 
 func _on_b_drag_ended(value_changed):
-	var bb = b.value / 255
-	color_rect.color.b = bb
-	changed.emit(self, bb)
+	color_rect.color.b = colorChanged(b, line_b)
 
 func setColor(c):
 	if c == null: 
@@ -43,3 +49,15 @@ func setColor(c):
 
 func getColor():
 	return color_rect.color
+
+func _on_line_r_text_changed(new_text):
+	r.value = int(new_text)
+	_on_r_drag_ended(0)
+
+func _on_line_g_text_changed(new_text):
+	g.value = int(new_text)
+	_on_g_drag_ended(0)
+
+func _on_line_b_text_changed(new_text):
+	b.value = int(new_text)
+	_on_b_drag_ended(0)
